620e771215c64cc3d7ce11c016f6913905d94480
azoonex <amirjabic@gamil.com>
2024-12-13 16:38:03 -0800

    feat: (new feature ,dropdown = ?handle-outside-clicks)

--- src/components/DropDown-demo.tsx
+++ src/components/DropDown-demo.tsx
@@ -1,56 +1,69 @@
-import React, { useState } from "react";
-import "../assets/home.style.scss";
+import React, { useEffect, useRef, useState } from "react";
 import "../assets/dropdown.style.scss";
-import DropDown from "./ui/drop-down";
+import "../assets/home.style.scss";
 import { TypeDataDropDwon } from "../types/types";
+import DropDown from "./ui/drop-down";
 
 const DropDownDemo: React.FC<TypeDataDropDwon> = (props) => {
-    const { data } = props;
-    const [isOpen, setIsOpen] = useState<boolean>(true);
-    const [onselectLabel, setOnSelectLabel] = useState<string | null>("");
+  const { data } = props;
+  const [isOpen, setIsOpen] = useState<boolean>(true);
+  const [onselectLabel, setOnSelectLabel] = useState<string | null>(null);
 
-    return (
-        <DropDown.DropRoot>
-            <DropDown.DropTrigger
-                disabled={data.length < 0}
-                className={isOpen && data.length > 0 ? "open" : undefined}
-                onClick={() => setIsOpen((prev) => !prev)}>
-                {!onselectLabel ? "DropDown" : onselectLabel}{" "}
-                <span>
-                    <img
-                        width={20}
-                        height={20}
-                        src='./chevron-up.svg'
-                        className={`${
-                            isOpen && data.length > 0
-                                ? "close_content"
-                                : undefined
-                        } checkOpen`}
-                    />
-                </span>
-            </DropDown.DropTrigger>
-            {isOpen && data.length > 0 && (
-                <DropDown.dropContent>
-                    {data.map((i) => (
-                        <DropDown.DropItem
-                            key={i.id}
-                            onClick={() => {
-                                if (!onselect) {
-                                    setOnSelectLabel(i.label);
-                                } else {
-                                    setOnSelectLabel(null);
-                                }
-                            }}>
-                            {i.label}
-                            <span>
-                                {i.label === onselectLabel ? "✅" : undefined}
-                            </span>
-                        </DropDown.DropItem>
-                    ))}
-                </DropDown.dropContent>
-            )}
-        </DropDown.DropRoot>
-    );
+  const rootRef = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent | TouchEvent) => {
+      if (rootRef.current && !rootRef.current.contains(event.target as Node)) {
+        isOpen && setIsOpen(false);
+      }
+    };
+    document.addEventListener("click", handleClickOutside, true);
+    return () => {
+      document.removeEventListener("click", handleClickOutside, true);
+    };
+  }, [isOpen]);
+
+
+  return (
+    <DropDown.DropRoot ref={rootRef}>
+      <DropDown.DropTrigger
+        disabled={data.length < 0}
+        className={isOpen && data.length > 0 ? "open" : undefined}
+        onClick={() => setIsOpen((prev) => !prev)}
+      >
+        {!onselectLabel ? "DropDown" : onselectLabel}{" "}
+        <span>
+          <img
+            width={20}
+            height={20}
+            src="./chevron-up.svg"
+            className={`${
+              isOpen && data.length > 0 ? "close_content" : undefined
+            } checkOpen`}
+          />
+        </span>
+      </DropDown.DropTrigger>
+      {isOpen && data.length > 0 && (
+        <DropDown.dropContent>
+          {data.map((i) => (
+            <DropDown.DropItem
+              key={i.id}
+              onClick={() => {
+                if (!onselectLabel) {
+                  setOnSelectLabel((prev) => (prev = i.label));
+                } else {
+                  setOnSelectLabel((prev) => prev = '');
+                }
+              }}
+            >
+              {i.label}
+              <span>{i.label === onselectLabel ? "✅" : undefined}</span>
+            </DropDown.DropItem>
+          ))}
+        </DropDown.dropContent>
+      )}
+    </DropDown.DropRoot>
+  );
 };
 
 export default DropDownDemo;
